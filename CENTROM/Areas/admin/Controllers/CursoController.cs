7c3e2405424dca12b380555761b39c2c7f981037
romelhuaraca <30274489+romelhuaraca@users.noreply.github.com>
2017-08-02 22:57:24 -0500

    Version 1

---
+++ CENTROM/Areas/admin/Controllers/CursoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CENTROM.Models;
+
+namespace CENTROM.Areas.admin.Controllers
+{
+    public class CursoController : Controller
+    {
+        private TiendaEntities db = new TiendaEntities();
+
+        // GET: admin/Curso
+        public ActionResult Index()
+        {
+            return View(db.Curso.ToList());
+        }
+
+        // GET: admin/Curso/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curso curso = db.Curso.Find(id);
+            if (curso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curso);
+        }
+
+        // GET: admin/Curso/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: admin/Curso/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CursoId,Denominacion,Descripcion,Precio,Activo")] Curso curso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Curso.Add(curso);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(curso);
+        }
+
+        // GET: admin/Curso/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curso curso = db.Curso.Find(id);
+            if (curso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curso);
+        }
+
+        // POST: admin/Curso/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CursoId,Denominacion,Descripcion,Precio,Activo")] Curso curso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(curso).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(curso);
+        }
+
+        // GET: admin/Curso/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Curso curso = db.Curso.Find(id);
+            if (curso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(curso);
+        }
+
+        // POST: admin/Curso/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Curso curso = db.Curso.Find(id);
+            db.Curso.Remove(curso);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CENTROM/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CENTROM.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Models.TiendaEntities bd = new Models.TiendaEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cursos( String id="")
+        {
+            //acceso a la base de datos
+            var cursos = bd.Curso.Where(x => x.Denominacion.Contains(id))
+                .Take(9)
+                .ToList();
+            ViewBag.palabra = id;
+            return View(cursos);
+        }
+
+        public ActionResult Eventos()
+        {
+            return View();
+        }
+
+        public ActionResult Carrito()
+        {
+            return View();
+        }
+
+        public ActionResult Nosotros()
+        {
+            return View();
+        }
+
+        public ActionResult Contacto()
+        {
+            return View();
+        }
+
+    }
+}
